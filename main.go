1ab86ce0a225356f209d2fbaed25265859f50dca
Andy O'Neill <andy@potatoriot.com>
2014-12-30 23:33:49 -0500

    Remove 'action' clutter

--- main.go
+++ main.go
@@ -11,23 +11,19 @@ import (
 	"github.com/werkshy/pickup/config"
 	"github.com/werkshy/pickup/handlers"
 	"github.com/werkshy/pickup/model"
-	"github.com/werkshy/pickup/player"
 	flag "launchpad.net/gnuflag"
 )
 
 func main() {
 	t0 := time.Now()
-	var action = flag.String("action", "serve", "Action to perform (serve|refresh)")
 	conf := config.Config{}
 	conf.MusicDir = flag.String("music-dir", "/music", "Music dir")
 	conf.Port = flag.Int("port", 8080, "Pickup port")
 	conf.MpdAddress = flag.String("mpd-address", "localhost:6600", "MPD address")
 	conf.MpdPassword = flag.String("mpd-password", "", "MPD Password")
 
-	var query = flag.String("query", "", "Search query")
 	flag.Parse(true)
 
-	log.Println("Action is:", *action)
 	log.Printf("Mpd address: '%s'  password: '%s'\n", *conf.MpdAddress,
 		*conf.MpdPassword)
 
@@ -36,20 +32,7 @@ func main() {
 	music := <-mpdChannel
 	log.Printf("Player with %d categories initialized in %v\n", len(music.Categories), time.Since(t0))
 
-	switch *action {
-	case "stats":
-		stats(mpdChannel)
-	case "search":
-		search(mpdChannel, *query)
-	case "serve":
-		serve(&conf, mpdChannel)
-	case "test-playback":
-		testPlayback(mpdChannel, &conf)
-	case "refresh":
-		os.Exit(0)
-	default:
-		fmt.Println("Unknown action", *action)
-	}
+	serve(&conf, mpdChannel)
 }
 
 func initializeMpd(mpdChannel chan *model.Collection, conf *config.Config) {
@@ -96,11 +79,13 @@ func serve(conf *config.Config, mpdChannel chan *model.Collection) bool {
 	artistHandler := handlers.ArtistHandler{mpdChannel}
 	playlistHandler := handlers.PlaylistHandler{mpdChannel, conf}
 	controlHandler := handlers.NewControlHandler(conf)
+
 	http.Handle("/categories/", categoryHandler)
 	http.Handle("/albums/", albumHandler)
 	http.Handle("/artists/", artistHandler)
 	http.Handle("/playlist/", playlistHandler)
 	http.Handle("/control/", controlHandler)
+
 	staticDir, _ := os.Getwd()
 	staticDir = staticDir + "/static"
 	log.Printf("Serving static files from %s\n", staticDir)
@@ -110,50 +95,7 @@ func serve(conf *config.Config, mpdChannel chan *model.Collection) bool {
 		http.FileServer(http.Dir(staticDir))))
 	http.HandleFunc("/", handlers.Index)
 	var bind = fmt.Sprintf(":%d", *conf.Port)
-	fmt.Printf("Serving from %s on %s\n", *conf.MusicDir, bind)
+	log.Printf("Serving from %s on %s\n", *conf.MusicDir, bind)
 	http.ListenAndServe(bind, nil)
 	return true
-}
-
-func stats(mpdChannel chan *model.Collection) {
-	music := <-mpdChannel
-	category := music.Categories[0]
-	fmt.Printf("Stats: %d tracks, %d albums, %d artists\n",
-		len(category.Tracks), len(category.Albums),
-		len(category.Artists))
-}
-
-func search(mpdChannel chan *model.Collection, query string) {
-	music := <-mpdChannel
-	matching := model.Search(music, query)
-	fmt.Printf("Matches for '%s':\n", query)
-
-	fmt.Println("\nMatching Tracks:")
-	for _, track := range matching.Tracks {
-		fmt.Printf("%-40s (%-20s)\n", track.Name, track.Artist)
-	}
-
-	fmt.Println("\n\nMatching Albums:")
-	for _, album := range matching.Albums {
-		fmt.Printf("%-40s (%s)\n", album.Name, album.Artist)
-	}
-}
-
-func testPlayback(mpdChannel chan *model.Collection, conf *config.Config) error {
-	music := <-mpdChannel
-	playlist := player.NewMpdPlaylist(conf)
-	playlist.Clear()
-
-	// add any old album
-	album := music.Categories[0].Artists[3].Albums[2]
-	log.Printf("Playing album %s - %s\n", album.Artist, album.Name)
-	playlist.AddAlbum(album)
-
-	// get the contents of the playlist
-	currentTracks, err := playlist.List()
-	log.Printf("Current playlist: (%d tracks)", len(currentTracks))
-	for _, track := range currentTracks {
-		log.Printf("%s\n", track)
-	}
-	return err
 }
