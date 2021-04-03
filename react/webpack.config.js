ff644eebc3d69b8cf42927694fe4f35727d50352
Andy O'Neill <andy@potatoriot.com>
2021-04-03 11:59:54 -0400

    [Build] Enable source maps
    To debug problems, we need source maps.
    Introduce (env, argv) so that we can only build source maps in production.

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,64 +1,62 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const PrettierPlugin = require("prettier-webpack-plugin");
-
-
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
-  filename: "./index.html"
+  filename: "./index.html",
 });
 
 const copyPlugin = new CopyPlugin({
-  patterns: [
-        { from: 'src/assets', to: 'assets' },
-  ]
+  patterns: [{ from: "src/assets", to: "assets" }],
 });
 
 const hotLoader = new webpack.HotModuleReplacementPlugin();
 
-const prettierPlugin = new PrettierPlugin()
+const prettierPlugin = new PrettierPlugin();
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+module.exports = (_env, argv) => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: "url-loader",
+        },
+      ],
+    },
+    resolve: {
+      extensions: ["*", ".js", ".jsx"],
+    },
+    plugins: [htmlPlugin, hotLoader, copyPlugin, prettierPlugin],
+    output: {
+      publicPath: "/react-static",
+    },
+    devtool: argv.mode == "development" ? "eval-source-map" : "source-map",
+    devServer: {
+      contentBase: "./dist",
+      hot: true, // Hot reloading
+      historyApiFallback: true, // Make react router work by using index.html to handle 404s.
+      port: 8081,
+      // Proxy requests to /api/* to the go server on port 8080
+      proxy: {
+        "/api": {
+          target: "http://localhost:8080",
+          secure: false,
+        },
       },
-      {
-        test:/\.css$/,
-        use:['style-loader','css-loader']
-      },
-      {
-        test: /\.(png|jpg)$/,
-        loader: 'url-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  plugins: [htmlPlugin, hotLoader, copyPlugin, prettierPlugin],
-  output: {
-    publicPath: "/react-static"
-  },
-  devServer: {
-    contentBase: './dist',
-    hot: true,                 // Hot reloading
-    historyApiFallback: true,  // Make react router work by using index.html to handle 404s.
-    port: 8081,
-    // Proxy requests to /api/* to the go server on port 8080
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        secure: false
-      }
-    }
-  }
+    },
+  };
 };
-
