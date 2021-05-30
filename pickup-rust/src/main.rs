839889fce85c0e9c846e8583b6d86ac43c89bf0b
Andy O'Neill <andy@potatoriot.com>
2021-05-29 20:24:44 -0400

    Set default log level to INFO

--- pickup-rust/src/main.rs
+++ pickup-rust/src/main.rs
@@ -10,10 +10,11 @@ mod player;
 
 use app_state::AppState;
 use player::Player;
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let sender = spawn_player();
 
