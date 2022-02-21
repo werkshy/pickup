03d79b774922f593f1a1fc553a2de76808b627e8
Andy O'Neill <andy@potatoriot.com>
2022-02-21 09:41:07 -0500

    [Build] Convert web buildfrom webpack to vite
    Webpack dependencies need a lot of gardening. I've seen vite used with
    fewer packages and less config, it seems to have sensible defaults for a
    static react app.
    
    - Rename *.js to *.jsx for vite convention
    - Move index.html to root dir per vite convention
    - Import index.jsx from index.html
    - Add simple vite config
    - Remove webpack deps

---
+++ react/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+const { resolve } = require("path");
+
+export default defineConfig({
+  plugins: [],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  server: {
+    port: 8081,
+    strictPort: true,
+    // Proxy API calls to the go backend on port 8080
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
